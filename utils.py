ed1d15ddd709f5fcc622160200e7471e11a97fb7
Kevin Van Le <lekevinvan@gmail.com>
2024-02-02 16:34:09 +0100

    copy dataset for filtering instead of inplace filtering

---
+++ utils.py
@@ -0,0 +1,10 @@
+import shutil, errno
+
+
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc:
+        if exc.errno in (errno.ENOTDIR, errno.EINVAL):
+            shutil.copy(src, dst)
+        else: raise
